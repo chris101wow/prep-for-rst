f43e1301409efad4c2e2a8d696990f380a283f92
Chris Hannalla <chris.hannalla@stu.ocsb.ca>
2025-05-15 09:56:17 -0400

    message

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let grass = new Image();
+
+grass.src="images/water.png"
 const CANVAS = document.getElementById('game_canvas');
 const CTX = CANVAS.getContext('2d', {
     powerPreference: "high-performance"
@@ -5,16 +9,18 @@ const CTX = CANVAS.getContext('2d', {
 
 
 
-let grass = new Image();
 
-grass.src="images/grass.png"
 
-CTX.rect(20, 20, 150, 100);
-CTX.fillStyle = "red";
+CTX.fillRect(0, 0, 100, 100); // 100 pixels wide, 50 pixels tallCTX.fillStyle = "red";
 CTX.fill();
+function draw(){
 CTX.drawImage(grass,0,300);
-  
+for (let j = 0; j < 720;j+=60){  
 for (let i = 0;i < 1200;i+=60){
-    CTX.drawImage(grass,i,150);
+    CTX.drawImage(grass,i,j);
+}
+}
 }
 console.log("Canvas initialized",grass);
+
+draw()
